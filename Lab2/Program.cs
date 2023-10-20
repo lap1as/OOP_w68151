2e5739346a86929078d08946e988cab147ef1fa4
lap1as <kuzukoleg12@gmail.com>
2023-10-20 03:56:21 +0200

    init

---
+++ Lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Lab2.Constructions;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var construction = new Construction(103, 45, 2, 8, "Brik");
+            Console.WriteLine(construction.Height);
+        }
+    }
+}
